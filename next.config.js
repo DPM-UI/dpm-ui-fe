08e01f78a44d5e33e6fc03ff55121ac10ba9b70f
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-10-14 01:36:08 +0700

    Init next svg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withReactSvg = require("next-react-svg");
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        reactStrictMode: true,
+        swcMinify: true,
+        webpack(config, options) {
+            config.module.rules.push({
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ["@svgr/webpack"],
+            });
+
+            return config;
+        },
+    };
+    return nextConfig;
+};
