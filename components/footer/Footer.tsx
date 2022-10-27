b5301ec037b7b7d5268edd502b5852dedd2f5295
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-10-28 01:25:14 +0700

    Implement footer not responsive yet

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,33 @@
+import { Header, Body } from "@components";
+import { SOCIAL_MEDIA } from "@constants";
+import Link from "next/link";
+
+export const Footer = () => {
+    return (
+        <div>
+            <div className="h-72 bg-light-cyan w-full">
+                <div className="flex justify-between px-20 py-7">
+                    <div className="flex flex-col gap-6 w-80 text-blue-2">
+                        <Header preset="h4">Alamat</Header>
+                        <Body preset="p2">
+                            Gedung Pusat Kegiatan Mahasiswa UI (Pusgiwa) Lama Lantai 2 Jl. Prof. Dr. Fuad Hassan,
+                            Kukusan, Kota Depok, 16425
+                        </Body>
+                        <Header preset="h4">Kontak</Header>
+                        <Body preset="p2">Email : dpm@ui.ac.id</Body>
+                    </div>
+                    <div className="flex flex-col gap-6 text-blue-2">
+                        <Header preset="h4">Media Sosial</Header>
+                        <div className="flex gap-6">
+                            {SOCIAL_MEDIA.map(({ logo, url }) => (
+                                <Link href={url}>
+                                    <a>{logo}</a>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ constants/SocialMedia.tsx
@@ -0,0 +1,23 @@
+import InstagramIcon from "@icons/ic_instagram.svg";
+import LinkedinIcon from "@icons/ic_linkedin.svg";
+import LineIcon from "@icons/ic_line.svg";
+import YoutubeIcon from "@icons/ic_youtube.svg";
+
+export const SOCIAL_MEDIA = [
+    {
+        logo: <InstagramIcon />,
+        url: "https://instagram.com/dpm_ui",
+    },
+    {
+        logo: <LinkedinIcon />,
+        url: "https://www.linkedin.com/company/dpm-ui/",
+    },
+    {
+        logo: <LineIcon />,
+        url: "https://line.me/R/ti/p/@isy5595k",
+    },
+    {
+        logo: <YoutubeIcon />,
+        url: "https://www.youtube.com/user/dpmui",
+    },
+];
