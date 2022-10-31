6f6c2f83f69b9d284d963bf3b896076cafb60e5d
Marcellinus Elbert <51221428+marcellinuselbert@users.noreply.github.com>
2022-10-31 21:01:38 +0700

    Implement divisi page (#12)

---
+++ constants/divisi-member/Litbang.tsx
@@ -0,0 +1,25 @@
+import Darryl from "@images/divisi/litbang/darryl.svg";
+import Farhan from "@images/divisi/litbang/farhan.svg";
+import Yudhistira from "@images/divisi/litbang/yudhistira.svg";
+
+export const AnggotaLitbang = [
+    {
+        position: "Kepala Divisi",
+        name: "Darryl Raditya",
+        faculty: "FT 2020",
+        image: <Darryl />,
+    },
+
+    {
+        position: "Wakil Kepala Divisi 1",
+        name: "Farhan Aziz",
+        faculty: "FT 2019",
+        image: <Farhan />,
+    },
+    {
+        position: "Wakil Kepala Divisi 2",
+        name: "Yudhistira Thufail",
+        faculty: "FH 2019",
+        image: <Yudhistira />,
+    },
+];
