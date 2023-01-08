850b92239d1cbb5afbb8e25380b30b405c7b3b31
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-01-08 17:54:38 +0700

    Struktur organisasi responsive implemented

--- pages/struktur-organisasi/index.tsx
+++ pages/struktur-organisasi/index.tsx
@@ -7,12 +7,12 @@ import { User } from "@models";
 const StrukturOrganisasi = ({ user }: { user: User }) => {
     return (
         <>
-            <div className="mt-10 pb-52 mx-20">
+            <div className="mt-10 mobile:pb-24 desktop:pb-52 desktop:mx-20 mobile:mx-3">
                 <Header preset="h1" className="text-blue-2 text-center">
                     Struktur Organisasi
                 </Header>
                 <img src="/assets/images/struktur-organisasi.png" className="w-full h-auto mt-8" />
-                <div className="flex justify-between">
+                <div className="desktop:flex justify-between mobile:hidden">
                     <Navigation isLeft url="/dpm-ui" target="DPM UI 2022" />
 
                     <Navigation isLeft={false} url="/anggota-perwakilan" target="Anggota Perwakilan" />
